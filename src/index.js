fd4e3e3a15a57b1551083ec543199c5b6bc48ded
Matt Swezey <matt@mattswezey.net>
2018-02-11 19:38:34 -0600

    Fix css import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import './components/css/Main.css';
+import './css/Main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
